b9945d8d38f7d7ba56800aef0b78aa6b6a7ca4b3
Benjamin Schichtholz <benjamin.schichtholz@mni.thm.de>
2022-05-15 21:53:41 +0200

    Rename ext Workspace to b

--- .xmonad/lib/Constants.hs
+++ .xmonad/lib/Constants.hs
@@ -6,4 +6,4 @@ myFocusFollowsMouse = True
 myClickJustFocuses :: Bool
 myClickJustFocuses = False
 myWorkspaces :: [String] 
-myWorkspaces    = ["1","2","3","4","5","6","7","8","9","ext"]
+myWorkspaces    = ["1","2","3","4","5","6","7","8","9","b"]
